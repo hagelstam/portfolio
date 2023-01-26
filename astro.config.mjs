96b300ff780c7e677828f4add5f4d2236d690227
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2023-01-27 00:08:07 +0200

    Add theme toggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import astroI18next from 'astro-i18next';
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next()],
+  integrations: [tailwind(), astroI18next(), preact()],
 });
