0b153262cee7b2b9b5dd4ca049d309b14f15602e
MaximilianHagelstam <maximilian.hagelstam@gmail.com>
2023-02-04 14:05:44 +0200

    Scale back

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import astroI18next from 'astro-i18next';
 import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next(), preact()],
+  integrations: [tailwind(), preact()],
 });
