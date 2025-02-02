b0ef3908794b6162ca96ec8f7749b4ff3048e95c
Maximilian Hagelstam <maximilian.hagelstam@smartum.fi>
2025-02-02 21:49:31 +0200

    chore: upgrade to tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import tailwind from '@astrojs/tailwind';
+// @ts-check
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
--- tailwind.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.astro'],
-  darkMode: 'class',
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
